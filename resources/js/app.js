930e92977f4584fc9c3c523a746dd28238ea2d8a
xzunun <jmg201098@gmail.com>
2023-05-20 22:08:15 -0600

    Se agrego otro commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
+// Importación de dependencias.
 import './bootstrap';
+import '../sass/app.scss';
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+import jQuery from "jquery";
+import DataTable from 'datatables.net-bs5';
+import 'datatables.net-responsive-bs5';
+import language from 'datatables.net-plugins/i18n/es-MX.json';
+import 'animate.css';
+
+// Inicialización global de DataTable.
+const table = new DataTable(document.querySelector('#tabla'), {
+    reponsive: true,
+    language: language
+});
+
+// Nuestros modulos que se vayan generando.
+
